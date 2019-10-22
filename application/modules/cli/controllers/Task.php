e674c7f004f1dab3179601ad9ee9c524cf3eb893
zixsi <Dmitriy Limanskiy>
2019-10-22 09:57:01 +0300

    +

--- application/modules/cli/controllers/Task.php
+++ application/modules/cli/controllers/Task.php
@@ -17,27 +17,27 @@ class Task extends APP_Controller
 	{
 		echo 'START'.PHP_EOL;
 		
-		$list = $this->TasksHelper->getEmailTasks();
-		foreach($list as $item)
-		{
-			$status = false;
-			$item['data'] = json_decode($item['data'], true);
-			switch($item['event'])
-			{
-				case Action::REGISTRATION:
-					$status = $this->EmailHelper->registration($item['data']);
-					break;
-				case Action::FORGOT:
-					$status = $this->EmailHelper->forgot($item['data']);
-					break;
-				default:
-						// empty
-					break;
-			}
+		// $list = $this->TasksHelper->getEmailTasks();
+		// foreach($list as $item)
+		// {
+		// 	$status = false;
+		// 	$item['data'] = json_decode($item['data'], true);
+		// 	switch($item['event'])
+		// 	{
+		// 		case Action::REGISTRATION:
+		// 			$status = $this->EmailHelper->registration($item['data']);
+		// 			break;
+		// 		case Action::FORGOT:
+		// 			$status = $this->EmailHelper->forgot($item['data']);
+		// 			break;
+		// 		default:
+		// 				// empty
+		// 			break;
+		// 	}
 
-			if($status)
-				$this->TasksModel->setStatus($item['id'], 1);
-		}
+		// 	if($status)
+		// 		$this->TasksModel->setStatus($item['id'], 1);
+		// }
 
 		echo 'END'.PHP_EOL;
 	}
